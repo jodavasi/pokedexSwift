14ab32c30b464ad328f47e7367204fafb03c5e2c
Daniel Vargas <jodavasi@hotmail.com>
2020-01-24 22:21:45 -0600

    prueba

--- pokedexSwift/ViewController.swift
+++ pokedexSwift/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Changes")
+        
         // Do any additional setup after loading the view.
     }
 
